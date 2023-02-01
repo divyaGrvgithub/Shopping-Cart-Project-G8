ca55f5fd2860099c3cde0fb7334ac971aa00b63f
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2023-02-01 16:03:10 +0530

    just started

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = require("./router/route")
+const mongoose = require("mongoose")
+const app = express()
+
+app.use(express.json())
+
+mongoose.set("strictQuery",true)
+mongoose.connect({"mongodb+srv://divyamala_:Dt25042000knp@divyamala.0cofsch.mongodb.net/group8Database"},{
+    useNewUrlParser:true
+})
+
+.then(()=>console.log("MongoDb is Connected"))
+.catch((err=>console.log(err)))
+
+app.use("/",router)
+
+app.listen(process.env.Port||3000,()=>{
+    console.log("Express App Running On Port",+(process.env.Port||3000))
+})
