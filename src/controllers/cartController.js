5c4ba37f69ad79e3a0494036e15f6efc0ce40e48
Abhi-cal <iamabhimaity@gmail.com>
2023-02-06 23:05:39 +0530

    cart create and get api done

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -12,9 +12,9 @@ const createCart = async (req, res) => {
         .send({ status: false, message: "not valid userId" });
     if (Object.keys(data).length == 0)
       return res.status(400).send({ status: false, message: "empty body" });
-    console.log(cartId)
+    
     let cartExist = await cartModel.findById(cartId);
-    console.log(cartExist)
+   
     if (!cartExist) {
       let productPrice = await productModel.findOne({
       _id: productId,
@@ -42,37 +42,55 @@ const createCart = async (req, res) => {
         .status(201)
         .send({ status: true, message: "Success", data: create });
     } else {
-      const items = cartExist.items;
-      let totalPrice = cartExist.totalPrice      
-      let totalItems=cartExist.totalItems
-      let updatedItems = []
-      for (let item of items) {
-        const product = await productModel.findOne({
-          _id: item.productId,
-          isDeleted: false,
-        });
-        if (!product)
-          return res.status(404).send({ message: "no product found" });
-        totalPrice += product.price;
-        totalItems+=item.quantity
-      }
-      let updateCart = {
-        items: items,
-        totalPrice: totalPrice,
-        totalItems: totalItems
-      };
-      const createCart = await cartModel.findByIdAndUpdate(
-        cartExist._id,
-        { $set: updateCart  },
-        { new: true }
-      );
-      return res
-        .status(201)
-        .send({ status: true, message: "Success", data: createCart });
-    }
+      let totalPrice=cartExist.totalPrice
+      
+      const product=await productModel.findOne({_id:productId},{isDeleted:false});
+      totalPrice+=product.price;
+    
+      let quantity=1
+      for (const item of cartExist.items){
+         
+     if(item.productId.toString()===productId.toString()){
+        quantity= item.quantity+1;       
+        }}
+        if(quantity==1){
+         let newItem ={
+          productId:productId,
+          quantity: quantity
+         }
+      cartExist.items.push(newItem)
+        }
+        let updateCart = {
+          items: cartExist.items,
+          totalPrice: totalPrice,
+          totalItems: cartExist.items.length
+        }
+        const createCart = await cartModel.findOneAndUpdate(
+          {_id:cartExist._id},
+          { $set: updateCart  },
+          { new: true }
+        );
+        return res
+          .status(201)
+          .send({ status: true, message: "Success", data: createCart })
+        }
+      
+    
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
-};
+  }
 
-module.exports = { createCart };
+
+  const getCart = async (req,res)=>{
+    try{
+      const userId = req.params.userId;
+      if(!mongoose.isValidObjectId(userId)) return res.status(400).send({status:false,message:"invalid userId"})
+      let getCartData = await cartModel.findOne({userId}).populate({path:'items.productId',model:'Product',select:["title","price","currencyFormat","description","productImage"]})
+      if(!getCartData) return res.status(404).send({ status: false, message: "cart is empty" })
+      res.status(200).send({status:true,message:"Success",data:getCartData})
+    }catch(err){
+      res.status(500).send({ Status: false, message: err.message })
+    }
+  }
+module.exports = { createCart, getCart };
